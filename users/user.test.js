949a2390bc36749a233f7da9c0b6b28c87442dad
peposlawa <peposlawa@gmail.com>
2024-08-07 15:27:43 +0100

    ticket 5 completed

--- users/user.test.js
+++ users/user.test.js
@@ -1,7 +1,19 @@
-import { expect } from "vitest"
-import { getUsers } from "./users.model"
+import { test, expect } from "vitest"
+import request from "supertest";
+import app from "../app.js";
 
+// test.skip("GET /api/health works", () => {
+// });
 
-test("GET /api/health works", () => {
-
-})
+// Goal: make a GET request with Supertest to the /api/health
+ test("GET /api/health works", async () => {
+  const response = await request(app).get("/api/health");
+  expect(response.headers["content-type"]).toMatch(/json/);
+  // assert the response body is correct
+    expect(response.body).toEqual({
+        success: true,
+        payload: "API is running correctly",
+    });
+    // assert the response status code is correct
+    expect(response.statusCode).toBe(200);
+ });
